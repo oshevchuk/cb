5706676886abbcbbed6bf38f56721c0731958448
megadevice83 <megadevice83@gmail.com>
2018-01-27 15:41:53 +0200

    Initial commit

---
+++ get-trainer-profile.php
@@ -0,0 +1,24 @@
+<?php
+
+echo '{
+  "name" : "Philip Acre",
+  "skills" : [
+    "php",
+    "C#",
+    "html5"
+    ],
+  "education" : [
+    "University 1",
+    "University 1",
+    "University 1"
+  ],
+  "experience": [
+    "SoftGroup",
+    "Some work"
+  ],
+  certifications: [
+  ],
+  "about-me" : [
+    "lorem ipsum"
+  ]
+}';
