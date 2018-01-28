9c6df3a1bfd455a7fcc5a2c586a998a4bfaf8e63
oshevchuk <megadevice83@gmail.com>
2018-01-28 22:52:09 +0200

    js

--- get-trainer-profile.php
+++ get-trainer-profile.php
@@ -1,3 +1,6 @@
 <?php
 
-echo '{  "name" : "Philip Acre",  "skills" : [    "php",    "C#",    "html5"    ],  "education" : [    "University 1",    "University 1",    "University 1"  ],  "experience": [    "SoftGroup",    "Some work"  ],  certifications: [  ],  "about-me" : [    "lorem ipsum"  ]}';
+$arr=array();
+
+
+echo '{  "name" : "Philip Acre",  "skills" : [    "php",    "C#",    "html5"    ],  "education" : [    "University 1",    "University 1",    "University 1"  ],  "experience" : [    "SoftGroup",    "Some work"  ],  "certifications" : [  ],  "about-me" : [    "lorem ipsum"  ]}';
--- js/popups.js
+++ js/popups.js
@@ -3,23 +3,66 @@ $('.close').click(function () {
 });
 
 $('.active-btn').click(function (e) {
-    if(profile = $(this).data("profile")){
+    if (profile = $(this).data("profile")) {
         getProfile(profile);
     }
-    // if($(this).data("profile") != undefined){
-    //     console.log($(this).data("profile"));
-    // }
+    if (contact = $(this).data("contact")) {
+        getContact(contact);
+    }
 });
 
 
 function getProfile(id) {
-    //to do implement ID to request
-    $.get("get-trainer-profile.php", function(data, status){
-        // alert("Data: " + data + "\nStatus: " + status);
-        if(status=="success") {
-            var info = JSON.stringify(data);
+    $.get("get-trainer-profile.php?id="+id, function (data, status) {
+        if (status == "success") {
+            $('.os-modal.modal-skills').css("display", "flex").hide().fadeIn(500);
 
-            console.log(info);
+            var info = JSON.parse(data);
+            $('.os-name').html(info["name"]);
+            $('.skills').html('');
+            $.each(info["skills"], function (i, val) {
+                var newElement = document.createElement('li');
+                newElement.innerHTML = val;
+                $('.skills').append("<li><a href='#'>" + val + "</a></li>");
+            });
+            $('.education').html(info["education"].join(', '));
+            $('.experience').html(info["experience"].join(', '));
+            $('.certifications').html(info["certifications"].join(', '));
+            $('.about-me').html(info["about-me"].join('. '));
+        } else {
+            ShowErr();
         }
     });
+}
+function getContact(id) {
+    $.get("get-trainer-contact.php?id="+id, function (data, status) {
+
+        if (status == "success") {
+            $('.os-modal.modal-contact').css("display", "flex").hide().fadeIn(500);
+            var info = JSON.parse(data);
+            $('.os-name').html(info["name"]);
+            $('.skills').html('');
+            $.each(info["skills"], function (i, val) {
+                var newElement = document.createElement('li');
+                newElement.innerHTML = val;
+                $('.skills').append("<li><a href='#'>" + val + "</a></li>");
+            });
+
+            $('.email').html(info["e-mail"]);
+            $('.location').html(info["location"]);
+            $('.phone').html(info["phone"]);
+            $('.skype').html(info["skype"]);
+        } else {
+            ShowErr();
+        }
+    });
+}
+
+function ShowErr() {
+    $('.os-modal').find('.modal-body').hide();
+    $('.os-modal').find('.err').fadeIn(400);
+    setTimeout(function () {
+        $('.os-modal').find('.modal-body').show();
+        $('.os-modal').find('.err').hide();
+    }, 1000)
 }
